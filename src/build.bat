a4c24dccf4038a18c752ea2d97824562a3b8bb78
Juan Antonio Cano <jacanosalado@gmail.com>
2017-04-18 13:09:08 +0200

    xz upload

---
+++ src/build.bat
@@ -0,0 +1,7 @@
+msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x86"
+msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x64"
+msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="ARM"
+
+msbuild ManagedLZMA.sln /p:Configuration="Release" /p:Platform="Any CPU"
+
+nuget pack ManagedLZMA.nuspec
