435b4e970a495152cc04b317af9c1b694564ee9b
jacano <jacanosalado@gmail.com>
2017-05-01 18:21:10 +0200

    Source link support :DD

--- src/build.bat
+++ src/build.bat
@@ -1,12 +1,10 @@
-set VS2015="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 set VS2017="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
-if exist %VS2015% (call %VS2015%) else if exist %VS2017% (call %VS2017%)
+if exist %VS2017% (call %VS2017%)
 
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x86"
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x64"
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="ARM"
 
-nuget restore ManagedLZMA.sln
 msbuild ManagedLZMA.sln /p:Configuration="Release" /p:Platform="Any CPU"
 
-nuget pack ManagedXZLZMA.nuspec -Symbols
+nuget pack ManagedXZLZMA.nuspec -Version 1.0.2
