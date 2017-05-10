4ef2a91e095223b53f0fcc29e10c2db99cbe9f3e
Juan Antonio Cano <jacanosalado@gmail.com>
2017-05-10 11:45:31 +0200

    build refactor

--- src/build.bat
+++ src/build.bat
@@ -1,9 +1,9 @@
-set VS2017="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
-if exist %VS2017% (call %VS2017%)
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
 
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x86"
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x64"
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="ARM"
+msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="ARM64"
 
-nuget restore ManagedLZMA.sln
-msbuild ManagedLZMA.sln /p:Configuration="Release" /p:Platform="Any CPU"
+nuget restore ManagedXZLZMA.sln
+msbuild ManagedXZLZMA.sln /p:Configuration="Release" /p:Platform="Any CPU"
