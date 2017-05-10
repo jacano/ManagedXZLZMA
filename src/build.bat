432e6872777d1143ba7863d293fb61c728d3943a
Juan Antonio Cano <jacanosalado@gmail.com>
2017-05-10 12:04:15 +0200

    bat fix

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,5 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+set VS2017="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+if exist %VS2017% (call %VS2017%)
 
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x86"
 msbuild xz/project/liblzma.sln /p:Configuration="Release" /p:Platform="x64"
