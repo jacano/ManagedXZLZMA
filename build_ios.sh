f0940e2fd0233cdb07015fd2684cd7ebc48bdeb3
jacano <jacanosalado@gmail.com>
2017-10-02 13:19:48 +0200

    ios build working

--- build_ios.sh
+++ build_ios.sh
@@ -1,8 +1,8 @@
-pushd src/crunch.iOS
+pushd src/xzlzma.iOS
 
 xcodebuild -configuration Release -sdk iphonesimulator clean build
 xcodebuild -configuration Release -sdk iphoneos clean build
-lipo -create -output "build/libcrunch.a" "build/Release-iphoneos/libcrunch.a" "build/Release-iphonesimulator/libcrunch.a"
+lipo -create -output "build/libxzlzma.a" "build/Release-iphoneos/libxzlzma.a" "build/Release-iphonesimulator/libxzlzma.a"
 
 popd
 
