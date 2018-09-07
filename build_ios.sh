36a7e3414adfe19adaaa4b3bfaa60916bca5d744
Juan Antonio Cano <jacanosalado@gmail.com>
2018-09-08 01:05:48 +0200

    progress

--- build_ios.sh
+++ build_ios.sh
@@ -1,13 +1,13 @@
-pushd src/crunch.iOS
+pushd src/xzlzma.iOS
 
 xcodebuild -configuration Release -sdk iphonesimulator clean build
 xcodebuild -configuration Release -sdk iphoneos clean build
 
 mkdir -p fat
-lipo -create -output "fat/libcrunch.a" "build/Release-iphoneos/libcrunch.a" "build/Release-iphonesimulator/libcrunch.a"
+lipo -create -output "fat/libxzlzma.a" "build/Release-iphoneos/libxzlzma.a" "build/Release-iphonesimulator/libxzlzma.a"
 
 popd
 
 rm -rf artifacts/ios
 mkdir -p artifacts/ios
-mv src/crunch.ios/fat/ artifacts/ios
+mv src/xzlzma.ios/fat/ artifacts/ios
