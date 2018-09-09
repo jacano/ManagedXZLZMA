24273301500a83bdfd08ba2db99ff524bc338167
Juan Antonio Cano <jacanosalado@gmail.com>
2018-09-09 23:41:23 +0200

    Progress

--- src/xzlzma.Android/Android.mk
+++ src/xzlzma.Android/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcrunch
+LOCAL_MODULE := libxzlzma
+LOCAL_CFLAGS := HAVE_CONFIG_H;DLL_EXPORT;NDEBUG;_USRDLL;LIBLZMADLL_EXPORTS;
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../crunch/crn.cpp
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../crunch/
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../xz;$(LOCAL_PATH)/../xz/liblzma/common;$(LOCAL_PATH)/../xz/common;$(LOCAL_PATH)/../xz/liblzma/api;$(LOCAL_PATH)/../xz/liblzma/check;$(LOCAL_PATH)/../xz/liblzma/delta;$(LOCAL_PATH)/../xz/liblzma/lz;$(LOCAL_PATH)/../xz/liblzma/lzma;$(LOCAL_PATH)/../xz/liblzma/rangecoder;$(LOCAL_PATH)/../xz/liblzma/simple
 include $(BUILD_SHARED_LIBRARY)
 
