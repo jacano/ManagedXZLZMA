7545747d212894585972efe3ad735d01af01322d
Juan Antonio Cano <jacanosalado@gmail.com>
2018-09-10 09:11:36 +0200

    Fix

--- src/xzlzma.Android/Android.mk
+++ src/xzlzma.Android/Android.mk
@@ -2,12 +2,23 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libxzlzma
-LOCAL_EXPORT_CFLAGS += \
+LOCAL_CFLAGS += \
 	-DHAVE_CONFIG_H \
 	-DDLL_EXPORT \
 	-DNDEBUG \
 	-D_USRDLL \
 	-DLIBLZMADLL_EXPORTS
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/../xz \
+	$(LOCAL_PATH)/../xz/liblzma/common \
+	$(LOCAL_PATH)/../xz/common \
+	$(LOCAL_PATH)/../xz/liblzma/api \
+	$(LOCAL_PATH)/../xz/liblzma/check \
+	$(LOCAL_PATH)/../xz/liblzma/delta \
+	$(LOCAL_PATH)/../xz/liblzma/lz \
+	$(LOCAL_PATH)/../xz/liblzma/lzma \
+	$(LOCAL_PATH)/../xz/liblzma/rangecoder \
+	$(LOCAL_PATH)/../xz/liblzma/simple
 LOCAL_SRC_FILES += \
 	$(LOCAL_PATH)/../xz/common/tuklib_cpucores.c \
 	$(LOCAL_PATH)/../xz/common/tuklib_physmem.c \
@@ -81,16 +92,5 @@ LOCAL_SRC_FILES += \
 	$(LOCAL_PATH)/../xz/liblzma/simple/simple_encoder.c \
 	$(LOCAL_PATH)/../xz/liblzma/simple/sparc.c \
 	$(LOCAL_PATH)/../xz/liblzma/simple/x86.c
-LOCAL_EXPORT_C_INCLUDES += \
-	$(LOCAL_PATH)/../xz \
-	$(LOCAL_PATH)/../xz/liblzma/common \
-	$(LOCAL_PATH)/../xz/common \
-	$(LOCAL_PATH)/../xz/liblzma/api \
-	$(LOCAL_PATH)/../xz/liblzma/check \
-	$(LOCAL_PATH)/../xz/liblzma/delta \
-	$(LOCAL_PATH)/../xz/liblzma/lz \
-	$(LOCAL_PATH)/../xz/liblzma/lzma \
-	$(LOCAL_PATH)/../xz/liblzma/rangecoder \
-	$(LOCAL_PATH)/../xz/liblzma/simple
 include $(BUILD_SHARED_LIBRARY)
 
