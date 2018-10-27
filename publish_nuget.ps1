9af0544186d4b760e2a819930dbbbc2e977a0b71
Juan Antonio Cano <jacanosalado@gmail.com>
2018-10-27 14:28:27 +0200

    fix in VS

--- publish_nuget.ps1
+++ publish_nuget.ps1
@@ -1,6 +1,6 @@
 $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw
 if (-not (Test-Path env:NUGET_KEY)) { $env:NUGET_KEY = Get-Content .\secrets\nuget_key.txt -Raw }
 
-nuget.exe push nuget/ManagedXZLZMA.Bindings.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
-nuget.exe push nuget/ManagedXZLZMA.Native.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/
-nuget.exe push nuget/ManagedXZLZMA.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
+nuget.exe push nuget/ManagedXZLZMA.Bindings.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://api.nuget.org/v3/index.json
+nuget.exe push nuget/ManagedXZLZMA.Native.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://api.nuget.org/v3/index.json
+nuget.exe push nuget/ManagedXZLZMA.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://api.nuget.org/v3/index.json
